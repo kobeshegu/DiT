625f1e6d76b3dba8dd04826508849a72ec1fd5ac
kobeshegu <mpyang_ecust@163.com>
2023-02-25 14:02:36 +0800

    Done:add gen.sh

---
+++ gen.sh
@@ -0,0 +1,3 @@
+srun -p digitalcity -N1 --quotatype=reserved --job-name=DiT --gres=gpu:1 --cpus-per-task=16 \
+ torchrun --nnodes=1 --nproc_per_node=1 sample_ddp.py --model DiT-XL/2 --num-fid-samples 50000 \
+ --sample-dir /mnt/petrelfs/yangmengping/generate_data/ImageNet256/DiT \
